07db2e2fbd5b051765fccd23da632c606e69f574
Jujulodace <damien.deturck@etu.u-bordeaux.fr>
2020-09-19 15:17:20 +0200

    JS doc

--- bot.js
+++ bot.js
@@ -192,7 +192,7 @@ const setPrefix = (message, prefix) => {
  * @param {role} role 
  */
 const addGroupe = (member, role) => {
-    if (role.name == "TD1" || role.name == "TD2" || role.name == "TD3" || role.name == "TDA1" || role.name == "TDA2" || role.name == "TDA3" ||role.name == "TDA4") {
+    if (role.name == "TD1" || role.name == "TD2" || role.name == "TD3" || role.name == "TDA1" || role.name == "TDA2" || role.name == "TDA3" || role.name == "TDA4") {
         member.roles.add(role);
         //message.channel.send(`ajout du role ${role.name}`)
         log.send(`${logDate()} ajout du role ${role.name} a ${member.nickname}`)
@@ -251,46 +251,29 @@ const makeRole = (guild, name) => {
     log.send(`${logDate()} creation du role ${name}`)
 }
 
-const messageAddGroup = (message,name) =>{
-
-    addRole(name,message,message.author)
-}
-const addRole = (name,message,user) => {
-    switch (name) {
-        case "🥇" :case "TD1": 
-
-            addGroupe(message.guild.members.cache.get(user.id), message.guild.roles.cache.filter(role => role.name === "TD1").first())
-            break;
-        case "🥈" :case "TD2":
-            addGroupe(message.guild.members.cache.get(user.id), message.guild.roles.cache.filter(role => role.name === "TD2").first())
-            break;
-        case "🥉" :case "TD3": 
-            addGroupe(message.guild.members.cache.get(user.id), message.guild.roles.cache.filter(role => role.name === "TD3").first())
-            break;
-        case "1️⃣" :case "TDA1": 
-            addGroupe(message.guild.members.cache.get(user.id), message.guild.roles.cache.filter(role => role.name === "TDA1").first())
-            break;
-        case "2️⃣" :case "TDA2": 
-            addGroupe(message.guild.members.cache.get(user.id), message.guild.roles.cache.filter(role => role.name === "TDA2").first())
-            break;
-        case "3️⃣" :case "TDA3": 
-            addGroupe(message.guild.members.cache.get(user.id), message.guild.roles.cache.filter(role => role.name === "TDA3").first())
-            break;
-        case "4️⃣" :case "TDA4": 
-            addGroupe(message.guild.members.cache.get(user.id), message.guild.roles.cache.filter(role => role.name === "TDA4").first())
-            break;
-        case "❌": 
-            if(message.member.roles.cache.get("751806478029160510")){
-                message.member.roles.set([])
-            }else{
-                message.member.roles.set([])
-            }
-            
-            break;
-        default:
-            break;
+/**
+ * 
+ * @param {message} message 
+ * @param {string[]} mes 
+ * 
+ * gestion de la création de groupe
+ */
+const ManageGroup = (message, mes) => {
+    if (mes.length === 1)
+        messageGroup(message)
+    else if (mes.length === 2)
+        messageAddGroup(message, mes[1])
+    else {
+        message.channel.send("nombre d'arguments invalide")
     }
+    message.delete({ timeout: 3000 })
 }
+/**
+ * 
+ * @param {message} message 
+ * 
+ * ajout/suppression groupe par reaction
+ */
 const messageGroup = (message) => {
     message.channel.send(groupEmbed).then(message => {
         message.react("🥇")
@@ -309,7 +292,7 @@ const messageGroup = (message) => {
         collector.on('collect', (reaction, user) => {
             console.log(`Collected ${reaction.emoji.name} from ${user.id}`);
             const name = reaction.emoji.name
-            addRole(name,message,user)
+            addRole(name, message, user)
         });
 
         collector.on('end', collected => {
@@ -317,15 +300,60 @@ const messageGroup = (message) => {
         });
     })
 }
-const ManageGroup = (message, mes) => {
-    if (mes.length === 1)
-        messageGroup(message)
-    else if (mes.length === 2)
-        messageAddGroup(message,mes[1])
-    else {
-        message.channel.send("nombre d'arguments invalide")
+
+
+/**
+ * 
+ * @param {string} name 
+ * @param {message} message 
+ * @param {user} user 
+ * 
+ * ajout du role 'name' a l'utilisateur 'user
+ */
+const addRole = (name, message, user) => {
+    switch (name) {
+        case "🥇": case "TD1":
+            addGroupe(message.guild.members.cache.get(user.id), message.guild.roles.cache.filter(role => role.name === "TD1").first())
+            break;
+        case "🥈": case "TD2":
+            addGroupe(message.guild.members.cache.get(user.id), message.guild.roles.cache.filter(role => role.name === "TD2").first())
+            break;
+        case "🥉": case "TD3":
+            addGroupe(message.guild.members.cache.get(user.id), message.guild.roles.cache.filter(role => role.name === "TD3").first())
+            break;
+        case "1️⃣": case "TDA1":
+            addGroupe(message.guild.members.cache.get(user.id), message.guild.roles.cache.filter(role => role.name === "TDA1").first())
+            break;
+        case "2️⃣": case "TDA2":
+            addGroupe(message.guild.members.cache.get(user.id), message.guild.roles.cache.filter(role => role.name === "TDA2").first())
+            break;
+        case "3️⃣": case "TDA3":
+            addGroupe(message.guild.members.cache.get(user.id), message.guild.roles.cache.filter(role => role.name === "TDA3").first())
+            break;
+        case "4️⃣": case "TDA4":
+            addGroupe(message.guild.members.cache.get(user.id), message.guild.roles.cache.filter(role => role.name === "TDA4").first())
+            break;
+        case "❌":
+            if (message.member.roles.cache.get("751806478029160510")) {
+                message.member.roles.set([])
+            } else {
+                message.member.roles.set([])
+            }
+            break;
+        default:
+            break;
     }
-    message.delete({ timeout: 3000 })
+}
+/**
+ * 
+ * @param {message} message 
+ * @param {string} name 
+ * 
+ * ajout role par message
+ */
+const messageAddGroup = (message, name) => {
+
+    addRole(name, message, message.author)
 }
 /**
  * restart toutes les 2h afin de vérifier l'activité du bot
