ec1c3288db930c3c70b4dc5b086cc8893d0f7005
sangi <jujulodace@gmail.com>
2020-09-21 11:43:52 +0200

    1 role TD a la fois, suppression de la reaction clear devenu inutile

--- group.js
+++ group.js
@@ -31,11 +31,10 @@ const messageGroup = (message) => {
         message.react("2️⃣")
         message.react("3️⃣")
         message.react("4️⃣")
-        message.react("❌")
         const filter = (reaction, user) => {
-            return ['🥇', '🥈', '🥉', '1️⃣', '2️⃣', '3️⃣', '4️⃣', '❌'].includes(reaction.emoji.name) && user.id !== message.author.id;
+            return ['🥇', '🥈', '🥉', '1️⃣', '2️⃣', '3️⃣', '4️⃣'].includes(reaction.emoji.name) && user.id !== message.author.id;
         };
-        const collector = message.createReactionCollector(filter);
+        const collector = message.createReactionCollector(filter, { dispose: true });
 
         collector.on('collect', (reaction, user) => {
             console.log(`Collected ${reaction.emoji.name} from ${user.id}`);
@@ -43,9 +42,12 @@ const messageGroup = (message) => {
             addRole(name, message, user)
         });
 
-        collector.on('end', collected => {
-            console.log(`Collected ${collected.size} items`);
+        collector.on('remove', (reaction, user) => {
+            console.log(`remove ${reaction.emoji.name} from ${user.id}`);
+            const name = reaction.emoji.name
+            removeRole(name, message, user)
         });
+
     })
 }
 
@@ -61,33 +63,78 @@ const messageGroup = (message) => {
 const addRole = (name, message, user) => {
     switch (name) {
         case "🥇": case "TD1":
+            removeGroupeTD(message.guild.members.cache.get(user.id))
             addGroupe(message.guild.members.cache.get(user.id), message.guild.roles.cache.filter(role => role.name === "TD1").first())
             break;
         case "🥈": case "TD2":
+            removeGroupeTD(message.guild.members.cache.get(user.id))
             addGroupe(message.guild.members.cache.get(user.id), message.guild.roles.cache.filter(role => role.name === "TD2").first())
             break;
         case "🥉": case "TD3":
+            removeGroupeTD(message.guild.members.cache.get(user.id))
             addGroupe(message.guild.members.cache.get(user.id), message.guild.roles.cache.filter(role => role.name === "TD3").first())
             break;
         case "1️⃣": case "TDA1":
+            removeGroupTDA(message.guild.members.cache.get(user.id))
             addGroupe(message.guild.members.cache.get(user.id), message.guild.roles.cache.filter(role => role.name === "TDA1").first())
             break;
         case "2️⃣": case "TDA2":
+            removeGroupTDA(message.guild.members.cache.get(user.id))
             addGroupe(message.guild.members.cache.get(user.id), message.guild.roles.cache.filter(role => role.name === "TDA2").first())
             break;
         case "3️⃣": case "TDA3":
+            removeGroupTDA(message.guild.members.cache.get(user.id))
             addGroupe(message.guild.members.cache.get(user.id), message.guild.roles.cache.filter(role => role.name === "TDA3").first())
             break;
         case "4️⃣": case "TDA4":
+            removeGroupTDA(message.guild.members.cache.get(user.id))
             addGroupe(message.guild.members.cache.get(user.id), message.guild.roles.cache.filter(role => role.name === "TDA4").first())
             break;
-        case "❌":
-            if (message.member.roles.cache.get("751806478029160510")) {
-                message.member.roles.set([])
-            } else {
-                message.member.roles.set([])
-            }
+
+        default:
             break;
+    }
+}
+
+/**
+ * 
+ * @param {string} name 
+ * @param {message} message 
+ * @param {user} user 
+ * 
+ * suppression du role 'name' a l'utilisateur 'user
+ */
+const removeRole = (name, message, user) => {
+    switch (name) {
+        case "🥇": case "TD1":
+
+            removeGroupe(message.guild.members.cache.get(user.id), message.guild.roles.cache.filter(role => role.name === "TD1").first())
+            break;
+        case "🥈": case "TD2":
+
+            removeGroupe(message.guild.members.cache.get(user.id), message.guild.roles.cache.filter(role => role.name === "TD2").first())
+            break;
+        case "🥉": case "TD3":
+
+            removeGroupe(message.guild.members.cache.get(user.id), message.guild.roles.cache.filter(role => role.name === "TD3").first())
+            break;
+        case "1️⃣": case "TDA1":
+
+            removeGroupe(message.guild.members.cache.get(user.id), message.guild.roles.cache.filter(role => role.name === "TDA1").first())
+            break;
+        case "2️⃣": case "TDA2":
+
+            removeGroupe(message.guild.members.cache.get(user.id), message.guild.roles.cache.filter(role => role.name === "TDA2").first())
+            break;
+        case "3️⃣": case "TDA3":
+
+            removeGroupe(message.guild.members.cache.get(user.id), message.guild.roles.cache.filter(role => role.name === "TDA3").first())
+            break;
+        case "4️⃣": case "TDA4":
+
+            removeGroupe(message.guild.members.cache.get(user.id), message.guild.roles.cache.filter(role => role.name === "TDA4").first())
+            break;
+
         default:
             break;
     }
@@ -116,6 +163,32 @@ const addGroupe = (member, role) => {
         log.send(`${logDate()} ajout du role ${role.name} a ${member.nickname}`)
     }
 }
+
+/**
+ * 
+ * @param {member} member 
+ * @param {role} role 
+ */
+const removeGroupe = (member, role) => {
+    if (role.name == "TD1" || role.name == "TD2" || role.name == "TD3" || role.name == "TDA1" || role.name == "TDA2" || role.name == "TDA3" || role.name == "TDA4") {
+        member.roles.remove(role)
+        log.send(`${logDate()} suppression du role ${role.name} a`)
+    }
+}
+const removeGroupeTD = (member) => {
+    removeGroupe(member, member.guild.roles.cache.filter(role => role.name === "TD1").first())
+    removeGroupe(member, member.guild.roles.cache.filter(role => role.name === "TD2").first())
+    removeGroupe(member, member.guild.roles.cache.filter(role => role.name === "TD3").first())
+
+}
+
+const removeGroupTDA = (member) => {
+    removeGroupe(member, member.guild.roles.cache.filter(role => role.name === "TDA1").first())
+    removeGroupe(member, member.guild.roles.cache.filter(role => role.name === "TDA2").first())
+    removeGroupe(member, member.guild.roles.cache.filter(role => role.name === "TDA3").first())
+    removeGroupe(member, member.guild.roles.cache.filter(role => role.name === "TDA4").first())
+}
+
 
 /**
  * retourne la date formater pour les logs
